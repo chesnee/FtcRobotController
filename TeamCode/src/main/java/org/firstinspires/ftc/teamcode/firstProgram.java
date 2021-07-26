51f70ca4dc45292159afc749ecaa00dcc05abacd
chesnee <chesnee.stewart@gmail.com>
2021-07-26 08:38:56 -0700

    firstRobotProgram

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstProgram.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+public class firstProgram {
+
+    public static void main(String args[]) {
+
+
+
+        int a = 1000;
+        int b = 1;
+
+        if (false) {
+            System.out.println("hello world");
+        } else if (false) {
+            System.out.println("The condition was false!");
+        } else {
+            System.out.println("No conditions were true");
+        }
+
+        }
+
+    }
